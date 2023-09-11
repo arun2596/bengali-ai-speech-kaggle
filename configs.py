1e15dffc0e2abbea3a9c408683ce709e10992ed6
Sudharshan Srinivasan <ssriniv2@saturn.nic.uoregon.edu>
2023-09-10 20:16:46 -0700

    model logs8sept

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
 SR = 16000 #wav2vec2 requires samplerate 16000
-ANNOTATION_FOLDER = 'data/raw/'
-TRAIN_DATA_FOLDER = 'data/raw/train_mp3s/'
+ANNOTATION_FOLDER = '/home/users/ssriniv2/learn-comp/data'
+TRAIN_DATA_FOLDER = '/home/users/ssriniv2/learn-comp/data/train_mp3s/'
 
 import os
 from transformers import TrainingArguments
@@ -18,26 +18,27 @@ class w2v_config:
 
     # Trainer arugments.
     trainer = TrainingArguments(
-      output_dir="model/output/run-001-wav2vec2-fulldata-cosine-lr3e-5",
+      output_dir="home/users/ssriniv2/learn-comp/model-out/output/8sept-001-arjith-300-fulldata-cosine-lr3e-5-ep3",
       group_by_length=False,
-      per_device_train_batch_size=8,
-      per_device_eval_batch_size=8,
+      per_device_train_batch_size=12,
+      per_device_eval_batch_size=12,
       ddp_find_unused_parameters = True,
       gradient_accumulation_steps=1,
       evaluation_strategy="steps",
-      num_train_epochs=10,
+      num_train_epochs=3,
       gradient_checkpointing=False,
       fp16=True,
-      save_steps=400,
-      eval_steps=400,
-      logging_steps=400,
+      save_steps=5000,
+      eval_steps=5000,
+      # eval_accumulation_steps = 256,
+      logging_steps=5000,
       learning_rate=3e-5,
-      dataloader_num_workers=os.cpu_count(),
-      warmup_steps=300,
+      dataloader_num_workers=24,
+      warmup_steps=1000,
       save_total_limit=10,
       push_to_hub=False,
-      run_name="run-001-wav2vec2-fulldata-cosine-lr3e-5",
+      run_name="8sept-001-arjith-300-fulldata-cosine-lr3e-5-ep3",
       load_best_model_at_end=True,
       lr_scheduler_type="cosine",
-      resume_from_checkpoint=True,
+      resume_from_checkpoint=False,
     )
